3632a34dbc3a6173d2b9d14d3d881d9260c4d757
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2021-06-08 20:35:45 +0300

    Fix to server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const jsonServer = require('json-server');
+const express = require('express');
+const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router('./src/db.json');
 const middlewares = jsonServer.defaults({
@@ -7,17 +9,15 @@ const middlewares = jsonServer.defaults({
 
 const port = process.env.PORT || 4000;
 
-server.use(middlewares, router);
-/* app.use('/db', middlewares, router);
-server.use(express.static(path.join(__dirname, 'build'))); */
 
-/* server.get('/*', function (req, res) {
+
+server.use('/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'build'))); 
+
+server.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const path = require('path');
-const express = require('express');
- */
 
 server.listen(port, () => {
 	console.log('Server is running');
